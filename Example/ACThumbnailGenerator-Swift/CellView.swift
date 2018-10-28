6dc5bd12c8b1889edf866f1fa78a63e938cbd457
Alejandro Cotilla <acotilla91@gmail.com>
2018-10-27 23:25:40 -0400

    Add `CellView` to use a custom full size image view.

---
+++ Example/ACThumbnailGenerator-Swift/CellView.swift
@@ -0,0 +1,13 @@
+//
+//  CellView.swift
+//  ACThumbnailGenerator-Swift_Example
+//
+//  Created by Alejandro Cotilla on 10/27/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CellView: UITableViewCell {
+    @IBOutlet weak var fullImageView: UIImageView!
+}
